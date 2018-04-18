97c6f39e6831172967f1eefaaf6b1af08b848e1a
wuzhicheng <wuzhicheng@zuzuche.com>
2018-04-18 12:15:41 +0800

    ajax

---
+++ worker/public/ajax-util.js
@@ -0,0 +1,175 @@
+/*
+ * Simple Ajax for Web Workers v0.1
+ * from https://github.com/pazguille/saw.js/edit/master/saw.js
+ */
+var workerScope = this,
+    saw = (function() {
+        var Ajax = function(conf) {
+            var that = this,
+                conf = conf ||  {},
+                methods = {
+                    "header": function(key, value) {
+                        conf.headers[key] = value;
+
+                        return methods;
+                    },
+                    "data": function(data) {
+                        conf.data = data;
+
+                        return methods;
+                    },
+                    "async": function(value) {
+                        conf.async = value;
+
+                        return methods;
+                    },
+                    "cache": function(value) {
+                        conf.cache = value;
+
+                        return methods;
+                    },
+                    "success": function(handler) {
+                        conf.success = handler;
+
+                        return methods;
+                    },
+                    "failure": function(handler) {
+                        conf.failure = handler;
+
+                        return methods;
+                    },
+                    "responseType": function(value) {
+                        conf.responseType = value;
+
+                        return methods;
+                    },
+                    "send": function() {
+                        var xhr = that.xhr;
+
+                        if (conf.jsonp) {
+                            that.setJSONP(conf.url, conf.success);
+
+                            return;
+                        }
+
+                        xhr.onreadystatechange = function() {
+                            that.stateChange(conf);
+                        };
+
+                        xhr.open(conf.method, conf.url, conf.async);
+                        that.setHeaders(conf.headers);
+                        xhr.responseType = conf.responseType;
+                        xhr.send(conf.data);
+
+                        return xhr;
+                    }
+                };
+
+            // Configuration
+            conf.headers = conf.headers ||  {};
+            conf.headers["Accept"] = "text/javascript, application/json, text/html, application/xml, text/xml, */*";
+            conf.headers["Cache-Control"] = "cache";
+            conf.headers["Content-Type"] = "application/x-www-form-urlencoded";
+            conf.responseType = ""; //"text", "arraybuffer", "blob", or "document"
+            conf.cache = true;
+            conf.async = true;
+
+            // XMLHttpRequest obj
+            this.xhr = new XMLHttpRequest();
+
+            return methods;
+        };
+
+        Ajax.prototype.setJSONP = function(url, handler) {
+            var that = this;
+            var callback = "_jsonp" + parseInt(new Date().getTime());
+
+            workerScope[callback] = function(data, status) {
+                workerScope[callback] = undefined;
+                workerScope[callback] = null;
+
+                return handler(data, status);
+            };
+
+            importScripts(url.replace("sawp", callback));
+        };
+
+        Ajax.prototype.stateChange = function(conf) {
+            var xhr = this.xhr;
+            if (xhr.readyState == 4) {
+                var response = xhr.response ||  xhr.responseText;
+
+                if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304 || (xhr.status == 0 && response)) {
+                    if (conf.hasOwnProperty("success")) {
+                        response = (conf.json) ? JSON.parse(response) : response;
+                        conf.success(response, xhr.status);
+                    }
+                } else {
+                    if (conf.hasOwnProperty("failure")) {
+
+                        var data = {
+                            "response": xhr.responseText,
+                            "status": xhr.status,
+                            "statusText": xhr.statusText
+                        }
+
+                        conf.failure(data);
+                    }
+                }
+
+            }
+        };
+
+        Ajax.prototype.setHeaders = function(headers) {
+            for (var key in headers) {
+                this.xhr.setRequestHeader(key, headers[key]);
+            };
+        };
+
+        var core = {
+
+            "get": function(url) {
+                var cnx = new Ajax({
+                    "method": "get",
+                    "url": url
+                });
+
+                return cnx;
+            },
+            "post": function(url) {
+                var cnx = new Ajax({
+                    "method": "post",
+                    "url": url
+                });
+
+                return cnx;
+            },
+            "json": function(url) {
+                var cnx = new Ajax({
+                    "method": "get",
+                    "url": url,
+                    "headers": {
+                        "Content-Type": "application/json"
+                    },
+                    "json": true
+                });
+
+                return cnx;
+            },
+            "jsonp": function(url) {
+                var cnx = new Ajax({
+                    "method": "get",
+                    "url": url,
+                    "headers": {
+                        "Content-Type": "application/json"
+                    },
+                    "jsonp": true
+                });
+
+                return cnx;
+            }
+        };
+
+        return core;
+
+    }());
--- worker/public/worker.js
+++ worker/public/worker.js
@@ -1,5 +1,5 @@
 //self来获取worker自带的方法.
-importScripts('./import-test.js');
+importScripts('./import-test.js', 'ajax-util.js');
 self.onmessage = function(e) {
     // console.log(e);
     console.log('worker接收到的内容是：', e.data);
@@ -7,3 +7,22 @@ self.onmessage = function(e) {
     self.close(); //关闭
 }
 test();
+
+// saw.json("ajax.json")
+//     .success(function(data) {
+//         postMessage(data);
+//     })
+//     .failure(function(data) {
+//         postMessage(data);
+//     })
+//     .send();
+
+
+// saw.jsonp("http://api.flickr.com/services/feeds/photos_public.gne?tags=cat&tagmode=any&format=json&jsoncallback=sawp")
+//     .success(function(data) {
+//         postMessage(data);
+//     })
+//     .failure(function(data) {
+//         postMessage(data);
+//     })
+//     .send();
